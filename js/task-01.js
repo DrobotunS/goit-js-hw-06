e3067cb263e35dec275272b34c0406b6dd429f26
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-09-01 22:58:36 +0300

    add1

---
+++ js/task-01.js
@@ -0,0 +1,4 @@
+const categoriesUl = document.querySelector('#categories');
+console.log(categoriesUl);
+const categoriesEl = categoriesUl.querySelectorAll('ul').length;
+console.log(`Number of categories: ${categoriesEl}`);

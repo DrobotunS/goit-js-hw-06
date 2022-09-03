0fefc3f2285de2d29a60bb12a0d57b1127e30e6c
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-09-03 18:12:57 +0300

    add3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,24 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const list = document.querySelector("ul.gallery");
+console.log(list);
+const element1 = document.createElement("li");
+const image1 = document.createElement("img");
+element1.append(image1);
+const image1Link = images[0];
+image1.src = image1Link.url
+
+const element2 = document.createElement("li");
+const image2 = document.createElement("img");
+element2.append(image2);
+const image2Link = images[1];
+image2.src = image2Link.url
+
+const element3 = document.createElement("li");
+const image3 = document.createElement("img");
+element3.append(image3);
+const image3Link = images[2];
+image3.src = image3Link.url
+
+list.append(element1, element2, element3);

37dc4d98190c623c89323f2eef10e0e6565e137f
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-09-09 17:37:04 +0300

    add10

--- js/task-09.js
+++ js/task-09.js
@@ -2,9 +2,16 @@ const handBtnRandom = document.querySelector('.change-color');
 const bodyRef = document.querySelector('body');
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+  return bodyRef.style.background = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+handBtnRandom.addEventListener('click', getRandomHexColor);
+  
 
-handBtnRandom.addEventListener('click', getRandomHexColor); 
-console.log(getRandomHexColor());
-bodyRef.style.background = `${getRandomHexColor()}`  
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// console.log(getRandomHexColor());
+// handBtnRandom.addEventListener('click', getRandomHexColor);
+//   bodyRef.style.background = `${getRandomHexColor()}`;
+
+// Задокументований код не працює, поясніть чому.
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,26 @@
+const inputRef = document.querySelector('#controls input');
+const createDtnRef = document.querySelector('[data-create]');
+const destroyDtnRef = document.querySelector('[data-destroy]');
+const divRef = document.querySelector('div#boxes');
+
+console.log(inputRef)
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return divRef.style.background = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+createDtnRef.addEventListener('click', getRandomHexColor);
+
+function createBoxes(amount) {
+  return event = amount.currentTarget.value;
+  const listItem = ingredients.map((ingredient) => {
+  const element = document.createElement("li");
+  element.textContent = 'he';
+  element.classList.add('item')
+  listEl.append(element);
+});
+  
+}
+inputRef.addEventListener("input", createBoxes); 
+    
+  
